8ab775a19cb85064efad1e99e17058157aca669f
geraldo tm <geraldomagcalastolentino@gmail.com>
2025-09-05 15:40:55 +0800

    feat: profile header

---
+++ components/profile/Profile.tsx
@@ -0,0 +1,35 @@
+import Text from "@/components/text/Text";
+import Feather from '@expo/vector-icons/Feather';
+import React from 'react';
+import { Image, Pressable, View } from 'react-native';
+import { useProfile } from './useProfile';
+
+const Profile = () => {
+    const { colors } = useProfile();
+    return (
+        <View className='py-3 px-6 flex-row items-center justify-between bg-base-100 dark:bg-base-100-dark'>
+            <View>
+                <Image
+                    source={require("@/assets/images/splash-icon.png")}
+                    className='w-10 h-10'
+                />
+            </View>
+            <View className='flex-row items-center gap-6'>
+                <Pressable className="active:opacity-40">
+                    <Feather name="globe" size={24} color={`${colors.baseContent}`} />
+                </Pressable>
+                <Pressable
+                    className="px-2 rounded-full border border-gray-300 active:opacity-40"
+                >
+                    <Text
+                        variant="h2"
+                        className="font-poppins" >
+                        G
+                    </Text>
+                </Pressable>
+            </View>
+        </View>
+    )
+}
+
+export default Profile;
---
+++ components/profile/useProfile.ts
@@ -0,0 +1,14 @@
+import { useThemeColor } from "@/hooks/useThemeColor";
+import { useTheme } from "@/lib/theme";
+import { useState } from "react";
+
+export const useProfile = () => {
+    const [isDarkMode, setIsDarkMode] = useState<string>("light");
+    const { colors } = useTheme();
+
+    return {
+        isDarkMode,
+        setIsDarkMode,
+        colors
+    }
+}
