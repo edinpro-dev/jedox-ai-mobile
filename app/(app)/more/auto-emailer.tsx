4b22a2544e2dc7fb1f6892fc1352590d9ca97f5f
geraldo tm <geraldomagcalastolentino@gmail.com>
2025-09-09 14:46:32 +0800

    feat: screens for auto-emailer driver-schedule and vehicle-history

---
+++ app/(app)/more/auto-emailer.tsx
@@ -0,0 +1,37 @@
+import { Button } from "@/components/button";
+import { Input } from "@/components/input";
+import { Select } from "@/components/select";
+import { Text } from "@/components/text";
+import { useAutoEmailer } from "@/features/more/hooks/autoEmailer/autoEmailer";
+import { IconPlus, IconSearch } from "@tabler/icons-react-native";
+import React from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const autoEmailer = () => {
+    const { colors, emailerTypeData } = useAutoEmailer();
+    return (
+        <SafeAreaView edges={["left", "right", "bottom"]} className="flex-1">
+            <View className="p-4">
+                <View className="py-2 border-b border-base-100 dark:border-base-100-dark">
+                    <View className="flex-row items-center justify-between">
+                        <Text>Emailer Setup</Text>
+                        <Button variant="primary">
+                            <View className="flex-row items-center gap-2">
+                                <IconPlus size={20} color="white" />
+                                <Text>New Email Config</Text>
+                            </View>
+                        </Button>
+                    </View>
+                </View>
+
+                <View className="mt-4 gap-4">
+                    <Input placeholder="Search" iconLeft={<IconSearch size={18} color={colors.infoContent} />} />
+                    <Select search={false} data={emailerTypeData} placeholder="Emailer Type" renderMode="checkbox" />
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default autoEmailer;
---
+++ app/(app)/more/vehicle-history.tsx
@@ -0,0 +1,43 @@
+import { Select } from "@/components/select";
+import { Text } from "@/components/text";
+import { useVehicleHistory } from "@/features/more/hooks/vehicleHistory/vehicleHistory";
+import { IconArrowUp, IconClipboard, IconSearch } from "@tabler/icons-react-native";
+import React from "react";
+import { View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const vehicleHistory = () => {
+    const { colors } = useVehicleHistory();
+    return (
+        <SafeAreaView edges={["left", "right", "bottom"]} className="flex-1">
+            <View className="flex-1 p-4">
+                <View className="border-b border-base-100 dark:border-base-100-dark">
+                    <Text variant={"h4"} className="py-1">
+                        Vehicle History
+                    </Text>
+                </View>
+
+                <View className="mt-4">
+                    <Select
+                        data={[]}
+                        variant="primary"
+                        placeholder="LICENSEPLATE"
+                        leftIcon={<IconSearch size={18} color={colors.infoContent} />}
+                    ></Select>
+                </View>
+
+                <View className="flex-1 flex-col items-center justify-center">
+                    <View className="flex-row items-center">
+                        <IconArrowUp size={70} color={colors.accent} />
+                        <IconClipboard size={70} color={colors.accent} />
+                    </View>
+                    <Text variant={"caption-large"} className="py-4">
+                        PLEASE SELECT DEALER LICENSE PLATE TO VIEW CONTENT
+                    </Text>
+                </View>
+            </View>
+        </SafeAreaView>
+    );
+};
+
+export default vehicleHistory;
